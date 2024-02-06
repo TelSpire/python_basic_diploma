1bfd6c916ff914dd4d258ef258fcb4bcede9cae6
Kirill <kirillvasengin@gmail.com>
2024-02-06 14:39:52 +0500

    Избавился от global переменных в функциях, и починил ошибку с отправкой билетов

--- loader.py
+++ loader.py
@@ -5,9 +5,9 @@ from config_data import config
 from states.mystates import MyStates
 
 
-base_request = config.request_params
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
-bot_type = None
 bot.add_custom_filter(custom_filters.StateFilter(bot))
+base_request = config.request_params
+cache = {}
 
